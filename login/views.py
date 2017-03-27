d8f2854594314f9dba5a4c385aef738e5346d5d0
Zhitao Li <lizt1994@163.com>
2017-03-27 10:51:18 +0800

    add socket communication

--- login/views.py
+++ login/views.py
@@ -1,4 +1,6 @@
 import datetime
+import socket
+import struct
 
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
@@ -20,25 +22,41 @@ def index(request):
     else:
         # undirect visit
         if (action=='login'):
-            try:
-                nid_post, passwd_post = request.POST['nid'], request.POST['passwd']
-            except KeyError:
-                return render(request, 'login/log_in_site.html', {'error_message':'NID or PASSWORD missed.'})
+            nid_post, passwd_post = request.POST['nid'], request.POST['passwd']
     
             # clear the obsolete login information
             for loginuser in LoginUser.objects.all():
                 if loginuser.time<timezone.now()-datetime.timedelta(days=1):
                     loginuser.delete()
-    
+                
             try:
                 user = LoginUser.objects.get(nid=nid_post)
             except LoginUser.DoesNotExist:
-                try:
-                    user = User.objects.get(nid=nid_post, passwd=passwd_post)
-                except User.DoesNotExist:
+                host = "1::1"
+                port = 9734
+                with socket.socket(socket.AF_INET6, socket.SOCK_STREAM) as s:
+                    s.connect((host, port))
+                    login_request = nid_post.encode("utf-8")
+                    if not (len(login_request)==10):
+                        return render(request, 'login/log_in_site.html', {'error_message': "Wrong NID or PASSWORD."})
+                    login_request += b'\0'+passwd_post.encode("utf-8")
+                    if (len(login_request)>=32):
+                        return render(request, 'login/log_in_site.html', {'error_message': "Wrong NID or PASSWORD."})
+                    while (len(login_request)<32):
+                        login_request += b'\0'
+                    #login_request += socket.inet_pton(socket.AF_INET6, request.META["REMOTE_ADDR"])
+                    login_request += socket.inet_pton(socket.AF_INET6, "1::2")
+                    s.send(login_request)
+                    data = s.recv(1024)
+                    succeed = struct.unpack("<L", data[:4])[0]
+                    nid = data[4:15].decode('utf-8')
+                    lip = socket.inet_ntop(socket.AF_INET6, data[16:])
+                    print(succeed, nid, lip)
+                    
+                if not (succeed):
                     return render(request, 'login/log_in_site.html', {'error_message': "Wrong NID or PASSWORD.",})
                 else:
-                    loginuser = LoginUser(nid=nid_post, lip=request.META['REMOTE_ADDR'], time=timezone.now())
+                    loginuser = LoginUser(nid=nid_post, lip=lip, time=timezone.now())
                     loginuser.save()
                     return render(request, 'login/log_in_succeed.html', {'nid':nid_post})
             else:
