0a458839c892dcd4795ede1af09ba7ce258276aa
Zhitao Li <lizt1994@163.com>
2017-04-17 19:43:16 +0800

    modify settings and views' logic

--- login/views.py
+++ login/views.py
@@ -16,7 +16,7 @@ def index(request):
         # direct visit
         nid = search_login_user(request.META['REMOTE_ADDR'])
         if nid:
-            return render(request, 'login/log_in_succeed.html', {'nid':nid})
+            return login_success(request, nid)
         else:
             return render(request, 'login/log_in_site.html')
     else:
@@ -38,27 +38,26 @@ def index(request):
                     s.connect((host, port))
                     login_request = nid_post.encode("utf-8")
                     if not (len(login_request)==10):
-                        return render(request, 'login/log_in_site.html', {'error_message': "Wrong NID or PASSWORD."})
+                        return login_failure(request)
                     login_request += b'\0'+passwd_post.encode("utf-8")
                     if (len(login_request)>=32):
-                        return render(request, 'login/log_in_site.html', {'error_message': "Wrong NID or PASSWORD."})
+                        return login_failure(request)
                     while (len(login_request)<32):
                         login_request += b'\0'
-                    #login_request += socket.inet_pton(socket.AF_INET6, request.META["REMOTE_ADDR"])
-                    login_request += socket.inet_pton(socket.AF_INET6, "1::2")
+                    login_request += socket.inet_pton(socket.AF_INET6, request.META["REMOTE_ADDR"])
                     s.send(login_request)
                     data = s.recv(1024)
                     succeed = struct.unpack("<L", data[:4])[0]
                     nid = data[4:15].decode('utf-8')
                     lip = socket.inet_ntop(socket.AF_INET6, data[16:])
-                    print(succeed, nid, lip)
                     
                 if not (succeed):
-                    return render(request, 'login/log_in_site.html', {'error_message': "Wrong NID or PASSWORD.",})
+                    return login_failure(request)
                 else:
-                    loginuser = LoginUser(nid=nid_post, lip=lip, time=timezone.now())
+                    # TODO: lip should be changed to the generated ip
+                    loginuser = LoginUser(nid=nid_post, lip=request.META["REMOTE_ADDR"], time=timezone.now())
                     loginuser.save()
-                    return render(request, 'login/log_in_succeed.html', {'nid':nid_post})
+                    return login_success(request, nid_post)
             else:
                 return render(request, 'login/log_in_site.html', {'error_message': "This NID has already log in.",})
         else:
@@ -74,3 +73,9 @@ def search_login_user(ip):
         if (loginuser.lip==ip):
             return loginuser.nid
     return None
+
+def login_success(request, nid):
+    return render(request, "login/log_in_succeed.html", {"nid": nid})
+
+def login_failure(request):
+    return render(request, "login/log_in_site.html", {"error_message": "Wrong NID or PASSWORD."})
